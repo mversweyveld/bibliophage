5048d8e11517529210eb6791f6dedaa58b83abf8
Matt Versweyveld <matt_v@earthlink.net>
2011-12-20 00:14:10 -0800

    initial default rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bibliophage::Application.config.secret_token = 'f14c5655ff108410c600533cd786a6bdecfbb30935647a4b1936478afbbf1736461c348974d1efb88137bf2ffb08307fce8e12fbb11bb4f3d928e93cc9d8e4c0'
